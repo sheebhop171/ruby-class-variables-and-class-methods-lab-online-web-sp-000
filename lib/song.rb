27ddb2d0685ecd5a7b8bd078d217ad5edfb774de
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-19 01:01:50 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,42 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count +=1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def genre_count
+    genre_count = {}
+    @@genres.each do |genre, song|
+      genre_count[genre]
+    end
+
+  end
+
+  def artist_count
+
+  end
+
+end
