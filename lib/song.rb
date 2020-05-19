5349fc9e57ec5c343ca472a6dedb84c47739f2e1
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-19 03:37:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,8 @@ class Song
   def self.genre_count
     genre_hash = {}
     @@genres.each do |genre|
-      if genre_hash[genre] += 1
+      if genre_hash[genre]
+        
       else
       genre_hash[genre] = 1
       end
