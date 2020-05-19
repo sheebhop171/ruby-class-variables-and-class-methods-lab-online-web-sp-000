5cf571edee645fbe89fc4b440425868b0ca1f972
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-19 01:33:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,13 +33,13 @@ class Song
     new_hash = {}
       #iterate over genres array
     @@genres.each_key do |key|  
-      # populate new hash with key/value pairs 
+      # populate new hash with key/value pairs
       # check if hash already contains key of particular genres
-      # if yes, increment the value of that key by one 
-      # else create new key/value pair 
-      
+      # if yes, increment the value of that key by one
+      # else create new key/value pair
+
       if @@genres.has_key?(key)
-         new_hash << key.count 
+         new_hash << key.count
       else
       end
     end
