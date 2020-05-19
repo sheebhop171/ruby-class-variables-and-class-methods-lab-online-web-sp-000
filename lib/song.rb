932de3e83b102bded9972564a7060b993b148c83
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-19 02:01:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
 
   def self.genre_count
     binding.pry
-    new_hash = Hash[@@genres.map {|genre| [genre, genre.count]}]
+    new_hash = Hash[@@genres.map {|genre| [genre, genre.size]}]
       #if new_hash.has_key?(genre)
       #  genre.size + 1
   end
